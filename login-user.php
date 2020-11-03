19e3e646603bffbbb7a2addc9d0c8361f04d18ce
MinhTuan-ui <66501408+MinhTuan-ui@users.noreply.github.com>
2020-11-03 15:26:08 +0700

    Update login-user.php

--- login-user.php
+++ login-user.php
@@ -20,7 +20,7 @@ session_start();
               <li><a href="#home">Home</a></li>
               <li><a href="#" >About</a></li>
               <li><a href="#" >Contract</a></li>
-              <li><a href="logout.php" data-ajax="false">logout</a></li>
+              <li><a href="logout.php" data-ajax="false">Logout</a></li>
               <li><a href="#"><?php echo $_SESSION['email']?></a></li>
             </ul>
           </div>
