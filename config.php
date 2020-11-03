bc8e4c0dd52e35639a9ffaf1739e66401e1a56f4
MinhTuan-ui <66501408+MinhTuan-ui@users.noreply.github.com>
2020-11-03 15:27:59 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'ec2-35-169-254-43.compute-1.amazonaws.com');
-define('DB_USERNAME', 'dazeirjoefhoff');
-define('DB_PASSWORD', '734442ea98536d24bbdfa5a79537c6ee81f2b35674045a4ea084a541f3d13320');
-define('DB_NAME', 'd5qncfmsqo2iav');
+define('DB_SERVER', 'ec2-54-237-155-151.compute-1.amazonaws.com');
+define('DB_USERNAME', 'hdjogbillqqnyb');
+define('DB_PASSWORD', '80c00ced55a1bae1f3877ede69f6450f4f939fcd4de5b3de0f574a95225caecc');
+define('DB_NAME', 'd9mpedeea8avbs');
 
 /* Attempt to connect to MySQL database */
 $link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
